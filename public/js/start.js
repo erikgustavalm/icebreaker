925b855f6f10376a89a62670cd2f91412d297e2d
Travispersson <travispersson93@gmail.com>
2020-02-10 20:59:14 +0100

    started on ticket APP: start-screen #1

---
+++ public/js/start.js
@@ -0,0 +1,15 @@
+
+
+
+const vm = new Vue({
+    el: 'main',
+    data: {
+
+    },
+    methods: {
+        goToSite: function (link){
+            window.location.href = link
+        }
+    }
+})
+
