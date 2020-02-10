1594c963f1b4655eafa36f841872c7656707bbe7
Travispersson <travispersson93@gmail.com>
2020-02-10 14:28:18 +0100

    set up node dev env for our app

---
+++ app.js
@@ -0,0 +1,29 @@
+'use strict';
+
+// Require express, socket.io, and vue
+const express = require('express');
+const app = express();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+const path = require('path');
+
+
+// Pick arbitrary port for server
+const port = 3000;
+app.set('port', (process.env.PORT || port));
+
+// Serve static assets from public/
+app.use(express.static(path.join(__dirname, 'public/')));
+// Serve vue from node_modules as vue/
+app.use('/vue',
+  express.static(path.join(__dirname, '/node_modules/vue/dist/')));
+// Serve index.html directly as root page
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname, 'public/index.html'));
+});
+
+
+/* eslint-disable-next-line no-unused-vars */
+const server = http.listen(app.get('port'), function() {
+  console.log('Server listening on port ' + app.get('port'));
+});
