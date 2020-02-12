0218a03c5c4bcd6e6579a0df9845210b790f9ffd
erikgustavalm <erikgustavalm@gmail.com>
2020-02-12 03:37:06 +0100

    Finally something that looks almost as planned

--- public/js/manager-pairs-wrapper.js
+++ public/js/manager-pairs-wrapper.js
@@ -1,21 +1,10 @@
 const mpw = new Vue({
     el: "#manager-pairs-wrapper",
     data: {
-	pairs: [],
 	users: users_json,
-	slots: 20,
+	slots: slots,
     },
     methods: {
-	showProfile: function(user) {
-	    console.log(user.name, user.age);
-	    console.log(user.img)
 
-	    if (user.showInfo) {
-		user.showInfo = false;
-	    } else {
-		user.showInfo = true;
-	    }
-	},
-
-     },
+    },
 })
