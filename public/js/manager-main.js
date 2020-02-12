87aeb4732f02a8deb57d360e465d4acb776f58dc
joakim hansson <skooben@skooben>
2020-02-12 17:42:52 +0100

    Added basic functinality to open a modal box when a button is clicked.

--- public/js/manager-main.js
+++ public/js/manager-main.js
@@ -1,1 +1,7 @@
 // Here we write json and functionalities.
+function openProfile() {
+    document.querySelector('.manager-profile-popup-bg').style.display = "flex";
+};
+function closeProfile() {
+    document.querySelector('.manager-profile-popup-bg').style.display = "none";
+};
