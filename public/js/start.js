49a1eff1dd5bf4e1c78b781bb91d4eb47968233f
Travis Persson <travispersson93@gmail.com>
2020-02-16 13:07:53 +0100

    login issue#10 (#25)

--- public/js/start.js
+++ public/js/start.js
@@ -4,12 +4,29 @@
 const vm = new Vue({
     el: '#app-container',
     data: {
-
+        userLoggedIn : false,
+        isHidden: true,
+        password: "",
+        username: "",
     },
     methods: {
         goToSite: function (link){
             window.location.href = link;
+        },
+        pullDown: function (){
+            console.log("account button clicked!");
+            if(this.isHidden){
+                this.isHidden = false;
+            } else {
+                this.isHidden = true;
+            }
+        },
+        loginUser: function(){
+            this.userLoggedIn = true;
+            this.isHidden = true;
+            console.log("user logged in successfully!");
         }
+
     }
 })
 
