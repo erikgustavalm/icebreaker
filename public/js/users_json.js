a3fda1d3822ff78b9f795b9950bcc97acb1b109c
JoakimHansson <41271920+JoakimHansson@users.noreply.github.com>
2020-02-17 13:35:50 +0100

    Done with issue 12 and 26 (#31)

--- public/js/users_json.js
+++ public/js/users_json.js
@@ -2,37 +2,43 @@ let users_json = [
     {
 	name: "Henrik",
 	age: 32,
-	img: "https://cdn.pixabay.com/photo/2017/06/13/12/53/profile-2398782_960_720.png",
-	showInfo: false
+	img: "https://i.kym-cdn.com/entries/icons/original/000/003/619/ForeverAlone.jpg",
+	showInfo: false,
+	id: "user0"
     },
     {
 	name: "Bengt",
 	age: 45,
-	img: "https://cdn.pixabay.com/photo/2017/06/13/12/53/profile-2398782_960_720.png",
-	showInfo: false
+	img: "https://i.kym-cdn.com/entries/icons/original/000/000/091/TrollFace.jpg",
+	showInfo: false,
+	id: "user1"
     },
     {
 	name: "Börje",
 	age: 21,
 	img: "https://cdn.pixabay.com/photo/2017/06/13/12/53/profile-2398782_960_720.png",
-	showInfo: false
+	showInfo: false,
+	id: "user2"
     },
     {
 	name: "Anna",
 	age: 82,
 	img: "https://cdn.pixabay.com/photo/2017/06/13/12/53/profile-2398782_960_720.png",
-	showInfo: false
+	showInfo: false,
+	id: "user3"
     },
     {
 	name: "Inger",
 	age: 17,
 	img: "https://cdn.pixabay.com/photo/2017/06/13/12/53/profile-2398782_960_720.png",
-	showInfo: false
+	showInfo: false,
+	id: "user4"
     },
     {
 	name: "Lotta",
 	age: 42,
 	img: "https://cdn.pixabay.com/photo/2017/06/13/12/53/profile-2398782_960_720.png",
-	showInfo: false
+	showInfo: false,
+	id: "user5"
     }
 ]
