d9ccc9b178d78cac3eb0b8639b3e54323efd176c
erikgustavalm <erikgustavalm@gmail.com>
2020-02-17 11:56:50 +0100

    Switched so that only one part of the creation is shown each time.

--- public/js/manager-host-session-form.js
+++ public/js/manager-host-session-form.js
@@ -1,16 +1,39 @@
 const mhsf = new Vue({
     el: "#manager-host-session-form",
     data: {
-	session: {name: ""},
-	quotes: [],
-	newQuote: ""
+	session: {name: "", quotes: [], questions: []},
+	newQuote: "",
+	newQuestion: "",
+	formNr: 0,
+	formLast: 2
    },
     methods: {
+
 	addQuote: function() {
-	    this.quotes.push(this.newQuote);
-	    this.newQuote = "";
-	    console.log(this.quotes);
+	    if (this.newQuote != "") {
+		this.session.quotes.push(this.newQuote);
+		this.newQuote = "";
+		console.log(this.quotes);
+	    }
 	},
+
+	addQuestion: function() {
+	    if (this.newQuestion != "") {
+		this.session.questions.push(this.newQuestion);
+		this.newQuestion = "";
+		console.log(this.question);
+	    }
+	},
+
+	formQuestionNext: function() {
+	    if (this.session.questions.length == 0) {
+		alert("Please enter at least one question");
+	    } else {
+		this.formNr++;
+	    }
+
+	},
+
 	formDone: function() {
 	    if (this.session.name != "") {
 		console.log("session.name" + this.session.name);
@@ -18,6 +41,13 @@ const mhsf = new Vue({
 	    } else {
 		console.log("Form not filled");
 	    }
+	},
+
+	formNext: function() {
+	    if (this.formNr == this.formLast) {
+		this.formDone();
+	    }
+	    this.formNr++;
 	}
     }
 })
