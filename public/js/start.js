730368501f629f26898a7f091cbfd28acd9edbb7
Travispersson <travispersson93@gmail.com>
2020-02-18 10:37:34 +0100

    moving profile registration to start

--- public/js/start.js
+++ public/js/start.js
@@ -2,19 +2,21 @@
 
 
 const vm = new Vue({
-    el: '#app-container',
+    el: '#start-site',
     data: {
         userLoggedIn : false,
         isHidden: true,
         password: "",
         username: "",
+        registrationHidden: true,
+        accountDetails: true,
     },
     methods: {
         goToSite: function (link){
             window.location.href = link;
         },
         pullDown: function (){
-            console.log("account button clicked!");
+            console.log("my account button clicked!");
             if(this.isHidden){
                 this.isHidden = false;
             } else {
@@ -25,7 +27,36 @@ const vm = new Vue({
             this.userLoggedIn = true;
             this.isHidden = true;
             console.log("user logged in successfully!");
+        },
+        logoutUser: function() {
+            this.userLoggedIn = false;
+            console.log("user logged out successfully!");
+        },
+        setRegistration: function() {
+            this.registrationHidden = false;
+            console.log("sign up button clicked!")
+        },
+        regProfile: function() {
+            this.accountDetails = false;
+            console.log("next button clicked!")
+        },
+        abortRegistration: function() {
+            this.accountDetails = true;
+            this.registrationHidden = true;
+            this.isHidden = true;
+            console.log("aborted registration!")
+        },
+        backToAccountDetails: function(){
+            this.accountDetails = true;
+            console.log("back to account details!")
+        },
+        doneWithRegistration: function(){
+            this.accountDetails = true;
+            this.registrationHidden = true;
+            this.isHidden = true;
+            console.log("Registration successfull!")
         }
+
 
     }
 })
