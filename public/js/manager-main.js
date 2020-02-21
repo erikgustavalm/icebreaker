314fd8ab040dd3d3e720d1f3b34ce4cc9cf0baf2
erikgustavalm <erikgustavalm@gmail.com>
2020-02-21 02:32:00 +0100

    Implemented swiping functinality for last stage page

--- public/js/manager-main.js
+++ public/js/manager-main.js
@@ -1,39 +1,37 @@
 // Here we write json and functionalities.
+
 let tables = [
     {
-	id1: 1, user1: "",  id2: 2, user2: "" 
+	id1: 1, user1: "", id2: 2, user2: ""
     },
     {
-	id1: 3, user1: "",  id2: 4, user2: "" 
+	id1: 3, user1: "", id2: 4, user2: ""
     },
     {
-	id1: 5, user1: "", id2: 6, user2: "" 
+	id1: 5, user1: "", id2: 6, user2: ""
     },
     {
-	id1: 7, user1: "", id2: 8, user2: "" 
+	id1: 7, user1: "", id2: 8, user2: ""
     },
     {
-	id1: 9, user1: "", id2: 10, user2: "" 
+	id1: 9, user1: "", id2: 10, user2: ""
     },
     {
-	id1: 11, user1: "", id2: 12, user2: "" 
+	id1: 11, user1: "", id2: 12, user2: ""
     },
     {
-	id1: 13, user1: "", id2: 14, user2: "" 
+	id1: 13, user1: "", id2: 14, user2: ""
     },
     {
-	id1: 15, user1: "", id2: 16, user2: "" 
+	id1: 15, user1: "", id2: 16, user2: ""
     },
     {
-	id1: 17, user1: "", id2: 18, user2: "" 
+	id1: 17, user1: "", id2: 18, user2: ""
     },
     {
-	id1: 19, user1: "", id2: 20, user2: "" 
+	id1: 19, user1: "", id2: 20, user2: ""
     }
 ]
-
-let idContainer;
-// let mouseOverActive = false;
 
 function onDragStart(event) {
     event.dataTransfer.setData("text", event.target.id);
@@ -54,55 +52,11 @@ function onDragExit(event) {
 function onDrop(event) {
     event.preventDefault();
     const id = event.dataTransfer.getData("text");
+
     const draggableElement = document.getElementById(id);
     draggableElement.style.backgroundColor = "blue";
     const dropzone = event.target;
     dropzone.classList.remove("manager-slot-hover");
     dropzone.appendChild(draggableElement);
      //event.dataTransfer.clearData();
-}
-
-function openProfile(event) {
-
-    // if(mouseOverActive != mouseOverCall) return;
-    // mouseOverActive = true;
-    
-    event.preventDefault();
-    console.log(event.parent);
-    console.log(event);
-    idContainer = event.target.id; // TODO: dont use global variable
-
-    const popupWindow = document.getElementById("manager-profile-popup");
-    const profile = document.getElementById(event.target.id);
-    let name = document.getElementById("manager-profile-popup-name");
-    let age = document.getElementById("manager-profile-popup-age");
-    let img = document.getElementById("manager-profile-popup-img");
-    let index = parseInt(profile.id.substring(4, profile.id.length), 10);
-    img.src = users_json[index].img;   
-    name.innerHTML = "Name: " + users_json[index].name;
-    age.innerHTML = "Age: " + users_json[index].age;
-
-    
-    /* Disable draggable while profile window is active */
-    profile.draggable = false;
-    
-    /* Make profile window visibel */
-    document.querySelector('.manager-profile-popup').style.display = "flex";
-    
-    /* Place profile window relative to the profile picture*/
-    popupWindow.style.top = profile.offsetTop + 'px';
-    popupWindow.style.left = (profile.offsetLeft + (profile.offsetWidth/2)) - (popupWindow.offsetWidth/2) + 'px';
-}
-
-function closeProfile(event) {
-    event.preventDefault();
-    
-    const id = idContainer;
-    const profile = document.getElementById(id);
-    
-    profile.draggable = true;
-    
-    document.querySelector('.manager-profile-popup').style.display = "none";
-
-    // if(mouseOverActive != mouseOverCall) mouseOverActive = false;
 }
--- public/js/users_json.js
+++ public/js/users_json.js
@@ -2,43 +2,37 @@ let users_json = [
     {
 	name: "Henrik",
 	age: 32,
-	img: "https://i.kym-cdn.com/entries/icons/original/000/003/619/ForeverAlone.jpg",
-	showInfo: false,
-	id: "user0"
+	img: "https://cdn.pixabay.com/photo/2017/06/13/12/53/profile-2398782_960_720.png",
+	showInfo: false
     },
     {
 	name: "Bengt",
 	age: 45,
-	img: "https://i.kym-cdn.com/entries/icons/original/000/000/091/TrollFace.jpg",
-	showInfo: false,
-	id: "user1"
+	img: "https://cdn.pixabay.com/photo/2017/06/13/12/53/profile-2398782_960_720.png",
+	showInfo: false
     },
     {
 	name: "Börje",
 	age: 21,
 	img: "https://cdn.pixabay.com/photo/2017/06/13/12/53/profile-2398782_960_720.png",
-	showInfo: false,
-	id: "user2"
+	showInfo: false
     },
     {
 	name: "Anna",
 	age: 82,
 	img: "https://cdn.pixabay.com/photo/2017/06/13/12/53/profile-2398782_960_720.png",
-	showInfo: false,
-	id: "user3"
+	showInfo: false
     },
     {
 	name: "Inger",
 	age: 17,
 	img: "https://cdn.pixabay.com/photo/2017/06/13/12/53/profile-2398782_960_720.png",
-	showInfo: false,
-	id: "user4"
+	showInfo: false
     },
     {
 	name: "Lotta",
 	age: 42,
 	img: "https://cdn.pixabay.com/photo/2017/06/13/12/53/profile-2398782_960_720.png",
-	showInfo: false,
-	id: "user5"
+	showInfo: false
     }
 ]
