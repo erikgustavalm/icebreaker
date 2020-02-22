9b942ef37b13a809a8fee87a02ab27b4f8ed5d8e
erikgustavalm <erikgustavalm@gmail.com>
2020-02-22 02:32:19 +0100

    Small fixes with animations

--- public/js/user-final.js
+++ public/js/user-final.js
@@ -34,25 +34,37 @@ const vm = new Vue ({
 	touchMove: function(event) {
 	    this.currX = event.touches[0].clientX;
 	    this.distance = Math.floor(this.startX - this.currX);
-	    if (this.distance > 200 || this.distance < -200) {
-		event.target.style.opacity = "0.1";
-	    } else if (this.distance > 150 || this.distance < -150) {
-		event.target.style.opacity = "0.25";
-	    } else if (this.distance > 100 || this.distnace < -100) {
-		event.target.style.opacity = "0.5";
-	    } else if (this.distance > 50 || this.distance < -50) {
-		event.target.style.opacity = "0.75";
-	    }
+	    // if (this.distance > 200 || this.distance < -200) {
+	    // 	event.target.style.opacity = "0.1";
+	    // } else if (this.distance > 150 || this.distance < -150) {
+	    // 	event.target.style.opacity = "0.25";
+	    // } else if (this.distance > 100 || this.distnace < -100) {
+	    // 	event.target.style.opacity = "0.5";
+	    // } else if (this.distance > 50 || this.distance < -50) {
+	    // 	event.target.style.opacity = "0.75";
+	    // }
 	},
 
 	touchEnd: function(event) {
-	    if (this.distance > this.limitX) {
-		event.target.style.backgroundColor = "red";
-	    } else if (this.distance < this.limitX * -1) {
-		event.target.style.backgroundColor = "green";
+
+	    if (!event.target.classList.contains("flipable")) {
+		this.distance = 0;
+		return;
 	    }
-	    event.target.style.opacity = "1";
+	    if (this.distance > this.limitX) {
+		event.target.style.animation = "flipper";
+		event.target.style.animationDuration = "0.2s";
+		event.target.style.animationTimingFunction = "linear";
+		event.target.style.animationFillMode = "forwards";
+	    } else if (this.distance < this.limitX * -1) {
+		event.target.style.animation = "flipper-back";
+		event.target.style.animationDuration = "0.2s";
+		event.target.style.animationTimingFunction = "linear";
+		event.target.style.animationFillMode = "forwards";
+	    }
+	    // event.target.style.opacity = "1";
 	    this.distance = 0;
+	    //event.target.style.animation = "None";
 	}
     }
 })
