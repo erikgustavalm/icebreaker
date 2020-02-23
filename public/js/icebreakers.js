75bcbd450da464d0065180131e97a17a7c9ea48c
Travispersson <travispersson93@gmail.com>
2020-02-23 19:16:23 +0100

    loading screen done

---
+++ public/js/icebreakers.js
@@ -0,0 +1,6 @@
+let icebreakers = [
+  "Did you know that Dv > IT?",
+  "Jockes nickname is skooben.. discuss.",
+  "markus name is sukram backwards, what's yours?",
+  "Sukram is an antivaxx- are u?"
+];
