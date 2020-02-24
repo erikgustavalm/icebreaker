2614ca3510ac951e21b9f3b2c48b49d7864e5047
kakann <43540525+kakann@users.noreply.github.com>
2020-02-24 11:03:25 +0100

    New questions and changed transition (#44)

--- public/js/rating.js
+++ public/js/rating.js
@@ -1,19 +1,25 @@
 //var VSlider = require('v-slider');
 
+window.setTimeout(
+    function(){
+        $('#current-date-img').fadeOut('slow');
+    },10000);
+
 const vm = new Vue({
   el: "#rate-date-container",
   data: {
-    questions: ["Q1", "Q2", "Q4", "Q5"],
+    questions: ["Was your date plesant?", "Did you find your date attractive?", "Would you like to meet your date again?"],
     defaultRating: 5,
     rating: ["5","5","5","5"],
     hideImg: false
   },
   methods: {
-    fadeImg: function() {
+    /*fadeImg: function() {
       img = document.getElementById("current-date-img");
       img.style.display = "none";
       this.hideImg = true;
     },
+*/
     submitRating: function(event){
         console.log(this.rating);
     }
