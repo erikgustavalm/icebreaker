8225c47b0f8183671803e811775d603d0f973ac1
erikgustavalm <erikgustavalm@gmail.com>
2020-02-26 12:36:03 +0100

    Quick fix in color scheme

--- public/js/manager-main.js
+++ public/js/manager-main.js
@@ -1,34 +1,34 @@
 // Here we write json and functionalities.
 let tables = [
     {
-	id1: 1, user1: "",  id2: 2, user2: "" 
+	id1: 1, user1: "",  id2: 2, user2: ""
     },
     {
-	id1: 3, user1: "",  id2: 4, user2: "" 
+	id1: 3, user1: "",  id2: 4, user2: ""
     },
     {
-	id1: 5, user1: "", id2: 6, user2: "" 
+	id1: 5, user1: "", id2: 6, user2: ""
     },
     {
-	id1: 7, user1: "", id2: 8, user2: "" 
+	id1: 7, user1: "", id2: 8, user2: ""
     },
     {
-	id1: 9, user1: "", id2: 10, user2: "" 
+	id1: 9, user1: "", id2: 10, user2: ""
     },
     {
-	id1: 11, user1: "", id2: 12, user2: "" 
+	id1: 11, user1: "", id2: 12, user2: ""
     },
     {
-	id1: 13, user1: "", id2: 14, user2: "" 
+	id1: 13, user1: "", id2: 14, user2: ""
     },
     {
-	id1: 15, user1: "", id2: 16, user2: "" 
+	id1: 15, user1: "", id2: 16, user2: ""
     },
     {
-	id1: 17, user1: "", id2: 18, user2: "" 
+	id1: 17, user1: "", id2: 18, user2: ""
     },
     {
-	id1: 19, user1: "", id2: 20, user2: "" 
+	id1: 19, user1: "", id2: 20, user2: ""
     }
 ]
 
@@ -66,21 +66,20 @@ function onDragStart(event) {
     }
 
     console.log(event.target.classList);
-    
+
     if(event.target.parentElement.classList.contains("manager-slot-empty")){
 	event.target.parentElement.style.opacity = 0.2;
 	console.log("looool");
     }
 
-    
     if(!event.target.classList.contains("manager-slot-taken") && !event.target.classList.contains("manager-list-slot")){
 	console.log("Invalid element!!");
 	event.preventDefault();
 	return;
     }
-    
+
     event.dataTransfer.setData("text", event.target.id);
-    event.currentTarget.style.backgroundColor = "yellow";
+    event.currentTarget.style.backgroundColor = "#eab9c1";
     startingSlot = event.currentTarget.parentNode;
     if(event.currentTarget.parentNode.getAttribute('name') == "profile-wrapper-slot"){
 	unlockSlot(event.currentTarget.parentNode);
@@ -114,21 +113,21 @@ function onDrop(event) {
 	console.log("Invalid element!!");
 	return;
     }
-    
-    draggableElement.style.backgroundColor = "#e3e3e3";
+
+    draggableElement.style.backgroundColor = "#e19ba7";
     let dropzone = event.currentTarget;
     dropzone.classList.remove("manager-slot-hover");
-    
+
     let userInfo = draggableElement.children[0];
     let userImg = draggableElement.children[1];
-    
+
     if(dropzone.id == "manager-user-list-wrapper"){
 	draggableElement.classList.remove("manager-slot-taken");
 	draggableElement.classList.add("manager-list-slot");
 	userInfo.style.display = "flex";
 	userImg.style.display = "none";
 	getUserById(draggableElement.id).matched = false;
-	
+
     }else if(dropzone.classList == "manager-list-slot"){
 	dropzone = document.getElementById("manager-user-list-wrapper");
 	draggableElement.classList.remove("manager-slot-taken");
@@ -142,7 +141,7 @@ function onDrop(event) {
 	    return;
 	}else{
 	    lockSlot(dropzone);
-	    draggableElement.classList.remove("manager-list-slot");	
+	    draggableElement.classList.remove("manager-list-slot");
 	    draggableElement.classList.add("manager-slot-taken");
 	    dropzone.style.opacity = "1";
 	    userInfo.style.display = "none";
@@ -150,7 +149,7 @@ function onDrop(event) {
 	    getUserById(draggableElement.id).matched = true;
 	}
     }
-  
+
     dropzone.appendChild(draggableElement);
     //event.dataTransfer.clearData();
 }
@@ -169,17 +168,17 @@ function openProfile(event) {
     let gender = document.getElementById("manager-profile-popup-gender");
     let img = document.getElementById("manager-profile-popup-img");
     let index = parseInt(profile.id.substring(4, profile.id.length), 10);
-    img.src = users_json[index].img;   
+    img.src = users_json[index].img;
     name.innerHTML = "Name: " + users_json[index].name;
     age.innerHTML = "Age: " + users_json[index].age;
     gender.innerHTML = "Gender: " + users_json[index].gender;
-    
+
     /* Disable draggable while profile window is active */
     profile.draggable = false;
-    
+
     /* Make profile window visibel */
     document.querySelector('.manager-profile-popup').style.display = "flex";
-    
+
     /* Place profile window relative to the profile picture*/
     popupWindow.style.top = profile.offsetTop + 'px';
     popupWindow.style.left = (profile.offsetLeft + (profile.offsetWidth/2)) - (popupWindow.offsetWidth/2) + 'px';
@@ -188,12 +187,12 @@ function openProfile(event) {
 function closeProfile(event) {
     console.log("closeProfile()");
     event.preventDefault();
-    
+
     const id = idContainer;
     const profile = document.getElementById(id);
-    
+
     profile.draggable = true;
-    
+
     document.querySelector('.manager-profile-popup').style.display = "none";
 
     // if(mouseOverActive != mouseOverCall) mouseOverActive = false;
@@ -209,7 +208,7 @@ function getUserById(userID){
 function swapUsers(draggableElement, dropzone){
 
     let element2 = dropzone.children[0];
-    
+
     if(draggableElement.classList == "manager-list-slot"){
 
 	let name1 = draggableElement.children[0];
@@ -217,16 +216,16 @@ function swapUsers(draggableElement, dropzone){
 
 	let name2 = element2.children[0];
 	let img2 = element2.children[1];
-	
-	
-	draggableElement.classList.remove("manager-list-slot");	
+
+
+	draggableElement.classList.remove("manager-list-slot");
 	draggableElement.classList.add("manager-slot-taken");
 
-	element2.classList.remove("manager-slot-taken");	
+	element2.classList.remove("manager-slot-taken");
 	element2.classList.add("manager-list-slot");
 	name1.style.display = "none";
 	img1.style.display = "block";
-	
+
 	name2.style.display = "block";
 	img2.style.display = "none";
 	getUserById(draggableElement.id).matched = true;
