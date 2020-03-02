96f533f5d0dd966fa13e2ba7a64f2f93360aa1be
Markus Lucero <markuslucero@hotmail.com>
2020-03-02 12:31:48 +0100

    a user can now join a server

--- public/js/join.js
+++ public/js/join.js
@@ -5,23 +5,37 @@ const vm = new Vue({
   el: "#join-room-container",
   data: {
     roomKey: "Enter room key:",
-    enteredKey: "",
-    loggedInUser: {},
-    
+    enteredKey: "", //eventID
+    loggedInUser: {}
   },
-  mounted(){
-    console.log(window.sessionStorage.getItem('roomId'));
-    socket.emit('requestUser', {
-      username: window.sessionStorage.getItem('roomId'),
+  mounted() {
+    console.log(window.sessionStorage.getItem("roomId"));
+    socket.emit("requestUser", {
+      username: window.sessionStorage.getItem("roomId")
     });
-    socket.on('getUser', function(user){
-      this.loggedInUser = user.data;
-      console.log(this.loggedInUser.username + " is logged in on this page");
-    }.bind(this));
+    socket.on(
+      "getUser",
+      function(user) {
+        this.loggedInUser = user.data;
+        console.log(this.loggedInUser.username + " is logged in on this page");
+      }.bind(this)
+    );
   },
   methods: {
+    joinRoom: function() {
+      socket.emit("joinEvent", {
+        user: this.loggedInUser,
+        eventID: this.enteredKey
+      });
+      socket.on("userJoined", function(data) {
+        window.sessionStorage.setItem("eventID", "data.eventID");
+        console.log(data.eventID);
+       
+      });
+      this.goToSite("../user/user_question_screen.html");
+    },
     goToSite: function(link) {
-        window.location.href = link;
+      window.location.href = link;
     }
   }
 });
