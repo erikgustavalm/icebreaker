7bdac9166e46186c5138c9037c230ce6e3171a0f
Travispersson <travispersson93@gmail.com>
2020-03-05 18:07:08 +0100

    will show table

--- public/js/manager-vue.js
+++ public/js/manager-vue.js
@@ -3,8 +3,8 @@ const socket = io();
 
 const vm = new Vue({
   el: "#manager-wrapper",
-    data: {
-        eventID: "",    
+  data: {
+    eventID: "",
     tables: tables,
     users: null,
     session: { session_name: "" },
@@ -14,131 +14,120 @@ const vm = new Vue({
     timePassed: 0,
     timeLeft: 0,
     timerInterval: 0,
-      pairs: null,
-      event: null
-      
+    pairs: null,
+    event: null
   },
   methods: {
-	autoMatch: function() {
-	    console.log("autoMatch()");
-	    if(this.users.length == 0){
-		console.log("No users to match, abort!");
-		return;
-	    }
-	    for(let i=0; i<this.users.length; i++){
-		if(this.users[i].matched == false){
-		    let pairIndex;
-		    let seat;
-		    if(i%2 == 0){
-	 		pairIndex = Math.floor(i/2);
-	 		seat = 0;
-	 	    }else{
-	 		pairIndex = Math.floor(i/2);
-	 		seat = 1;
-	 	    }
-		    this.moveUserToPair(this.users[i], pairIndex, seat);
-		}
-	    }
-
-	    socket.emit("onMatchingDone", {
-		table: this.table,
-	    });
-	},
-
-	moveUserToPair: function(user, pairIndex, seat) {
-	    const userElement = document.getElementById(user.id);
-	    console.log(userElement);
-	    const userName = userElement.children[0];
-	    const userImg = userElement.children[1];
-	    
-	    let pair = this.pairs[pairIndex].children;
-
-	    if(slotTaken(pair[seat]) == "true"){
-//		lockSlot(startingSlot);
-		console.log("Slot is taken, abort!!");
-		return;
-	    }
-
-	    userElement.classList.remove("manager-list-slot");
-	    userElement.classList.add("manager-slot-taken");
-	    userName.style.display = "none";
-	    userImg.style.display = "block";
-
-	    pair[seat].style.opacity = "1";
-
-
-	    // console.log(user.id);
-	    // console.log(this.pairs[pairIndex].lastChild.id);
-	    // console.log(seat);
-	    
-	    rearrange_table(user.id, getTableId(this.pairs[pairIndex].lastChild.id), seat+1);
-	    pair[seat].appendChild(userElement);
-	    lockSlot(pair[seat]);
-	    user.matched = true;
-	},
-
-    
-	switchShowHelp: function() {
-	    if (this.showHelp) {
-		this.showHelp = false;
-	    } else {
-		this.showHelp = true;
-	    }
-	    console.log("Switched show help");
-	},
-
-	onTimesUp: function() {
-	    clearInterval(this.timerInterval);
-	    this.timerLabel = "START ROUND";
-
-	    document.getElementById("manager-round-timer").style.backgroundColor = "#399939";
-
-	    this.timePassed = 0;
-	    this.timeLeft = 0;
-	},
-
-	startTimer: function() {
-	    this.timerInterval = setInterval(() => {
-		this.timePassed = this.timePassed += 1;
-		this.timeLeft = this.TIME_LIMIT - this.timePassed;
-		this.timerLabel = this.formatTime();
-
-		document.getElementById("manager-round-timer").style.backgroundColor = "#ff3939";
-
-		if (this.timeLeft === 0) {
-		    this.onTimesUp();
-		}
-	    }, 1000);
-
-	},
-
-	formatTime: function() {
-	    m = Math.floor(this.timeLeft / 60);
-	    s = this.timeLeft % 60;
-	    if (s < 10) {
-		s = `0${s}`;
-	    }
-	    return `${m}:${s}`;
-	},
-
-  sendMatchedPairs: function(){
-      var matched = [];
-      let i = 0;
-      for(let table of this.tables)
-      {
-          matched[i] = [table.seat1.id, table.seat2.id];
-          i++;
+    autoMatch: function() {
+      console.log("autoMatch()");
+      if (this.users.length == 0) {
+        console.log("No users to match, abort!");
+        return;
+      }
+      for (let i = 0; i < this.users.length; i++) {
+        if (this.users[i].matched == false) {
+          let pairIndex;
+          let seat;
+          if (i % 2 == 0) {
+            pairIndex = Math.floor(i / 2);
+            seat = 0;
+          } else {
+            pairIndex = Math.floor(i / 2);
+            seat = 1;
+          }
+          this.moveUserToPair(this.users[i], pairIndex, seat);
+        }
       }
 
-      console.log(matched);
-      console.log(this.eventID);
+      //* send seats to users
+      this.sendMatchedPairs();
+    },
+
+    moveUserToPair: function(user, pairIndex, seat) {
+      const userElement = document.getElementById(user.id);
+      console.log(userElement);
+      const userName = userElement.children[0];
+      const userImg = userElement.children[1];
+
+      let pair = this.pairs[pairIndex].children;
+
+      if (slotTaken(pair[seat]) == "true") {
+        //		lockSlot(startingSlot);
+        console.log("Slot is taken, abort!!");
+        return;
+      }
+
+      userElement.classList.remove("manager-list-slot");
+      userElement.classList.add("manager-slot-taken");
+      userName.style.display = "none";
+      userImg.style.display = "block";
+
+      pair[seat].style.opacity = "1";
+
+      // console.log(user.id);
+      // console.log(this.pairs[pairIndex].lastChild.id);
+      // console.log(seat);
+
+      rearrange_table(
+        user.id,
+        getTableId(this.pairs[pairIndex].lastChild.id),
+        seat + 1
+      );
+      pair[seat].appendChild(userElement);
+      lockSlot(pair[seat]);
+      user.matched = true;
+    },
+
+    switchShowHelp: function() {
+      if (this.showHelp) {
+        this.showHelp = false;
+      } else {
+        this.showHelp = true;
+      }
+      console.log("Switched show help");
+    },
+
+    onTimesUp: function() {
+      clearInterval(this.timerInterval);
+      this.timerLabel = "START ROUND";
+
+      document.getElementById("manager-round-timer").style.backgroundColor =
+        "#399939";
+
+      this.timePassed = 0;
+      this.timeLeft = 0;
+    },
+
+    startTimer: function() {
+      this.timerInterval = setInterval(() => {
+        this.timePassed = this.timePassed += 1;
+        this.timeLeft = this.TIME_LIMIT - this.timePassed;
+        this.timerLabel = this.formatTime();
+
+        document.getElementById("manager-round-timer").style.backgroundColor =
+          "#ff3939";
+
+        if (this.timeLeft === 0) {
+          this.onTimesUp();
+        }
+      }, 1000);
+    },
+
+    formatTime: function() {
+      m = Math.floor(this.timeLeft / 60);
+      s = this.timeLeft % 60;
+      if (s < 10) {
+        s = `0${s}`;
+      }
+      return `${m}:${s}`;
+    },
+
+    sendMatchedPairs: function() {
+	  console.log(this.tables);
       socket.emit("sendMatchedPairs", {
-          matchedPairs: matched,
-          eventID: this.eventID
+        matchedPairs: this.tables,
       });
-
-  },
-
+    },
 
     onTimesUp: function() {
       clearInterval(this.timerInterval);
@@ -183,14 +172,20 @@ const vm = new Vue({
       socket.emit("requestEvent", {
         eventID: window.sessionStorage.getItem("eventID")
       });
-	socket.on("getEvent", function(event) {
-	    this.event = event.event;
-	    this.users = this.event.users;
-	}.bind(this)
-		 );
+      socket.on(
+        "getEvent",
+        function(event) {
+          this.event = event.event;
+          this.users = this.event.users;
+        }.bind(this)
+      );
     }
 
-      socket.on("onUserJoin", function(data){
-	  this.users.push(data.user);
-      }.bind(this));
-  }});
+    socket.on(
+      "onUserJoin",
+      function(data) {
+        this.users.push(data.user);
+      }.bind(this)
+    );
+  }
+});
--- public/js/waiting.js
+++ public/js/waiting.js
@@ -4,48 +4,84 @@ const socket = io();
 const vm = new Vue({
   el: "#text-wrapper",
   data: {
-    numOfIcebreakers: icebreakers.length,
+    numOfIcebreakers: 0,
     hideIcebreaker: true,
     visible: false,
+    icebreakers: [],
     icebreaker: "",
     icebreakerIndex: 0,
     loggedInUser: {},
+    wait: true,
+    overlayOff: true
   },
-  mounted(){
-    console.log(window.sessionStorage.getItem('roomId'));
-    socket.emit('requestUser', {
-      username: window.sessionStorage.getItem('roomId'),
+  mounted() {
+    socket.emit("requestUser", {
+      username: window.sessionStorage.getItem("roomId")
     });
-    socket.on('getUser', function(user){
-      this.loggedInUser = user.data;
-      console.log(this.loggedInUser.username + " is logged in on this page");
-    }.bind(this));
+    socket.on(
+      "getUser",
+      function(user) {
+        this.loggedInUser = user.data;
+        console.log(this.loggedInUser.username + " is logged in on this page");
+      }.bind(this)
+    );
+
+    socket.emit("requestIcebreakers", {
+      roomId: window.sessionStorage.getItem("roomId"),
+      eventID: window.sessionStorage.getItem("eventID")
+    });
+
+    socket.on(
+      "sendIcebreakers",
+      function(data) {
+        this.icebreakers = data.icebreakers;
+        this.numOfIcebreakers = data.icebreakers.length;
+      }.bind(this)
+    );
+
+    socket.on(
+      "yourDate",
+      function(data) {
+        console.log(data.seat);
+        this.wait = false;
+        this.overlayOff = false;
+        this.setTable(data);
+      }.bind(this)
+    );
   },
   methods: {
     showIcebreaker: async function() {
-      var i = 0;
-      while (i < 2) {
+      while (this.wait) {
         this.visible = false;
         this.hideIcebreaker = true;
         await this.sleep(4000);
         this.hideIcebreaker = false;
-        this.icebreakerIndex = Math.floor(Math.random() * this.numOfIcebreakers);
+        this.icebreakerIndex = Math.floor(
+          Math.random() * this.numOfIcebreakers
+        );
         this.visible = true;
-        this.icebreaker = icebreakers[this.icebreakerIndex];
+        this.icebreaker = this.icebreakers[this.icebreakerIndex];
         await this.sleep(4000);
         this.icebreaker = "";
-        i++;
       }
-      this.goToSite("../user/dating.html");
-
     },
     sleep: function(ms) {
       return new Promise(resolve => setTimeout(resolve, ms));
     },
+    setTable: function(data) {
+      var seat = document.getElementById(data.seat.tableID);
+      seat.classList.add("colorIn"); 
 
-    goToSite: function (link){       
-    window.location.href = link
-},
+      if (data.seat.seat1.username === this.loggedInUser.username) {
+
+        
+      } else {
+      }
+    },
+
+    goToSite: function(link) {
+      window.location.href = link;
+    }
   },
   created() {
     this.showIcebreaker();
